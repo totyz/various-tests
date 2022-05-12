aa6f6db77f1b2afaabf3b421773b924969f7273d
Tomasz Tyzenhauz <tomasz.tyzenhauz@nordicsemi.no>
2022-05-12 11:17:54 +0200

    test6

--- tests/test6.py
+++ tests/test6.py
@@ -2,12 +2,17 @@
 # which run ls/dir command with folder as param and returns all files and folders in list form.
 # Print results one entry after another
 # What happens when invalid folder has been passed. How to secure program (in such situation we should get empty list?
+import subprocess
 from pathlib import Path
-from typing import List
 
 
-def get_files(folder: Path) -> List[str]:
-    pass
+def get_files(folder: Path):
+    try:
+        ret = subprocess.check_output(['ls', '-al', folder]).decode('utf-8').split('\n')
+    except subprocess.CalledProcessError:
+        ret = []
+    return ret
 
 if __name__ == '__main__':
-    print(get_files(Path('/tmp')))
+    for el in get_files(Path('/t')):
+        print(el)
