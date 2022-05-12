a53f30172b37d230e90c1dd831dc25a4080dc79a
Tomasz Tyzenhauz <tomasz.tyzenhauz@nordicsemi.no>
2022-05-12 11:08:45 +0200

    test4

--- tests/test4.py
+++ tests/test4.py
@@ -2,8 +2,8 @@
 # which takes the largest digit from string "4214210949214918248184128" and returns as Int.
 # What happens when string contains none digit character? How to modify function to get rid of such characters?
 
-def get_max(data: str) -> int:
-    pass
+def get_max(el: str):
+    return max(list(el))
 
 if __name__ == '__main__':
     print(get_max('12'))
