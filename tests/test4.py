073511218d3b254acffeb1f581e995cdce0f93f3
Tomasz Tyzenhauz <tomasz.tyzenhauz@nordicsemi.no>
2022-05-12 11:36:06 +0200

    test4 with fix

--- tests/test4.py
+++ tests/test4.py
@@ -3,7 +3,11 @@
 # What happens when string contains none digit character? How to modify function to get rid of such characters?
 
 def get_max(el: str):
-    return max(list(el))
+    return max(map(lambda x: int(x, 0), [x for x in set(el) if '\x30' <= x <= '\x39']))
+
+def get_max(x):
+    print(x)
+    return max([i for i in x if i.isdigit()])
 
 if __name__ == '__main__':
     print(get_max('12'))
