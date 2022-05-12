cdadee2b45408e0afd27dba25ab9f1a2aef00cd3
Tomasz Tyzenhauz <tomasz.tyzenhauz@nordicsemi.no>
2022-05-12 11:12:32 +0200

    test5

--- tests/test5.py
+++ tests/test5.py
@@ -2,11 +2,11 @@
 # which returns type of exception in string form
 
 def exception_type(ex: Exception) -> str:
-    pass
+    return type(ex).__name__
 
 if __name__ == '__main__':
     try:
         sum()
     except Exception as ex:
         print(ex)
-        print(exception_type(ex))  # here we should see TypeError
+        print(exception_type(ex))
