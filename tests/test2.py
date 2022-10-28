bfffb93780dfce77e19494256694b755c6918f30
Tomasz Tyzenhauz <tomasz.tyzenhauz@nordicsemi.no>
2022-10-28 11:15:52 +0200

    test2

--- tests/test2.py
+++ tests/test2.py
@@ -1,6 +1,19 @@
+# What the code does?
 # What will be the output?
+# how to fix it?
 if __name__ == '__main__':
-    d = [1, 2, 3, 4]
+    d = [3, 11, 5, 22, 4, 33, 5]
     for i, el in enumerate(d):
-        d.pop(i)
-    print(d)
+        if i > 0 and i % 2 == 0:
+            d.pop(i)
+    print(d)  # wrong, remove from enumerated list which become shorter
+
+    # Filtering by creating new list
+    n = []
+    for i, el in enumerate(d):
+        if i > 0 and i % 2 == 0:
+            n.append(el)
+    print(n)
+
+    # Filtering by list comp
+    print([el for i, el in enumerate(d) if i > 0 and i % 2 == 0])
