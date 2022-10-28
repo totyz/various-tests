876ad35cb929a083bea2437fe173a4897daecd49
Tomasz Tyzenhauz <tomasz.tyzenhauz@nordicsemi.no>
2022-10-28 11:08:57 +0200

    test2: change it

--- tests/test2.py
+++ tests/test2.py
@@ -1,6 +1,9 @@
+# What the code does?
 # What will be the output?
+# how to fix it?
 if __name__ == '__main__':
-    d = [1, 2, 3, 4]
+    d = [3, 11, 5, 22, 4, 33, 5]
     for i, el in enumerate(d):
-        d.pop(i)
+        if i > 0 and i % 2 == 0:
+            d.pop(i)
     print(d)
